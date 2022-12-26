61142b32ab601e1d3596e48d20f16205a251bab7
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-12-26 15:35:03 +0600

    :memo: Setting up the Users Model

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,8 +12,12 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
-func NewModel(db *sql.DB) Models {
-	return Models{Movies: MovieModel{DB: db}}
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
+	}
 }
