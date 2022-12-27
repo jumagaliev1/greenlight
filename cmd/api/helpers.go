67121e51016b9088e2a6816448e702c51ba6b5bd
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-12-27 19:53:47 +0600

    :recycle: Graceful Shutdown of Background Tasks

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -127,8 +127,10 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 func (app *application) background(fn func()) {
 	// Launch a background goroutine.
+	app.wg.Add(1)
 	go func() {
 		// Recover any panic.
+		defer app.wg.Done()
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/lib/pq"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -44,6 +45,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
